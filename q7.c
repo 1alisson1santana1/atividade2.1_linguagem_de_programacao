379b754cdce743ed3983479eba7f12babe646edb
1alisson1santana1 <awss3@aluno.ifal.edu.br>
2026-09-23 23:23:44 -0300

    Add program to convert seconds to hours, minutes, seconds

--- q7.c
+++ q7.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void) {
+    int total;
+    
+    printf("Total de segundos: ");
+    scanf("%d", &total);
+    
+    int horas = total / 3600;
+    int minutos = (total % 3600) / 60;
+    int segundos = total % 60;
+    
+    printf("%dh %dmin %ds\n", horas, minutos, segundos);
+    
+    return 0;
+}
